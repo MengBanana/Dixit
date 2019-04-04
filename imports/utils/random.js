362c4007dd46087e61a0d4dc9ff1053b2ccf45cb
MengBanana <xinmengzhang@utexas.edu>
2019-04-04 11:45:37 -0700

    shuffle cards

--- imports/utils/random.js
+++ imports/utils/random.js
@@ -1,37 +1,26 @@
+var shuffle = require("shuffle-array");
+
 export function random(cards, players) {
-  const totalCards = players*players*2;
+  shuffle(cards);
   let allInHand = [];
   let allDistributed = [];
   let res = [];
-  let cardsPerPerson = totalCards/players;
 
   for (let i=0; i<players; i++) {
     let cardsInHand = [];
     let cardsDistributed =[]; 
-    for (let j=0; j<cardsPerPerson; j++) {
+    for (let j=0; j<12; j++) {
       if (j % 2 === 0) {
-        cardsInHand.push(cards[i*cardsPerPerson+j]);
+        cardsInHand.push(cards[i*12+j]);
       }
       else {
-        cardsDistributed.push(cards[i*cardsPerPerson+j]);
+        cardsDistributed.push(cards[i*12+j]);
       }
     }
-    if (Math.random() < 0.5) {
-      allInHand.push(cardsInHand);
-      allDistributed.push(cardsDistributed);
-    }
-    else {
-      allInHand.push(cardsDistributed);
-      allDistributed.push(cardsInHand);
-    }
+    allInHand.push(cardsInHand);
+    allDistributed.push(cardsDistributed);
   }
-  if (Math.random() < 0.5) {
-    res.push(allInHand);
-    res.push(allDistributed);
-  }
-  else {
-    res.push(allDistributed);
-    res.push(allInHand);
-  }
+  res.push(allInHand);
+  res.push(allDistributed);
   return res; // array of arrays
 }
