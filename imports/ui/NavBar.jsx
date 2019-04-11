02e9015a9bf1cd899fac72b8b904f958666d423f
Yanwen Huangxu <huangxu.y@husky.neu.edu>
2019-04-11 12:35:35 -0700

    navbar changes and about page

--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -8,19 +8,28 @@ export default class NavBar extends Component {
     return (
       <div className="navbar-container">
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-            <div className="nav-item ml-auto signin">
+          <div className="nav-item ml-auto signin">
               <AccountsUIWrapper />
             </div>
-          </div>
           <div className="navbar-header">
-            <NavLink className="nav-link" activeClassName="active" to="/GameRoom">
+            <NavLink className="nav-link" activeClassName="active" to="/">
               <img className="navbar-brand" src="http://www.sophia-jezykiobce.pl/wp-content/uploads/2018/02/dixit-logo-1-1170x630.png" width="250px"/>
             </NavLink>
           </div>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            
+            <ul className="navbar-nav ml-auto navbar-expand-lg ">
+              <li className="nav-item">
+                <NavLink className="nav-link" activeClassName="active" to="/About">Game Rules</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" activeClassName="active" to="/gameroom">Let's Play</NavLink>
+              </li>
+            </ul>
+          </div>
           
         </nav>
       </div>
