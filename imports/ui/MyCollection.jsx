217e62e5746385de1f679df31a1cd87b1bae2d27
Yanwen Huangxu <huangxu.y@husky.neu.edu>
2019-04-26 01:12:09 -0700

    collection add card info

--- imports/ui/MyCollection.jsx
+++ imports/ui/MyCollection.jsx
@@ -8,6 +8,10 @@ class MyCollection extends Component {
   constructor(props) {
     super(props);
   }
+  getDate(createdAt) {
+    let data =  new Date(createdAt);
+    return data.toDateString();
+  }
 
   render() {
     console.log(this.props.myData);
@@ -17,24 +21,28 @@ class MyCollection extends Component {
       <div className="container collection">
         <h2 className="row collection"> My Collection 
           {this.props.myData != []?
-            <h5>
+            <span>
               <span id="badge" className="badge badge-pill badge-warning m-2">
                 {this.props.myData.map(m => (<h5 key={m}>    {m.collection.length} cards</h5>))}
               </span>
-            </h5> : null}
+            </span> : null}
         </h2>
         { this.props.myData != []?
-          <div className="row" id="addcard">
+          <div className="row collectionCards" id="addcard">
             {this.props.myData.map(m => m.collection.map(card => (
+              
               <div
+
                 key={card._id}
-                className="card col-xs-4 col-s-3"
+                className="card col-xs-4 col-s-3 container image"
                 style={{
                   backgroundImage: `url(${card.url})`,
                   backgroundSize: "cover"
                 }}
               >
-                
+                <div className="overlay">
+                  <div className="text">{this.getDate(card.createdAt)}<br/>@ {card.gameName}</div>
+                </div>
               </div>
             )))}
           </div> : null
